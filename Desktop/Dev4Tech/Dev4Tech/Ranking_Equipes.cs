0dd6f7ff3674110e7514495540819ed250e8dd45
gu1lh3rme-rodrigues <guilherme.robles@etec.sp.gov.br>
2025-08-31 15:11:20 -0300

    Versão JV

---
+++ Desktop/Dev4Tech/Dev4Tech/Ranking_Equipes.cs
@@ -0,0 +1,345 @@
+﻿// Ranking_Equipes.cs
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Dev4Tech
+{
+    public partial class Ranking_Equipes : Form
+    {
+        private EquipesRanking equipesRanking = new EquipesRanking();
+
+        public Ranking_Equipes()
+        {
+            InitializeComponent();
+            this.Load += Ranking_Equipes_Load;
+        }
+
+        private void Ranking_Equipes_Load(object sender, EventArgs e)
+        {
+            CarregarRankingEquipes();
+        }
+
+        private void CarregarRankingEquipes()
+        {
+            panelRankingEquipes.Controls.Clear();
+
+            DataTable dtEquipes = equipesRanking.BuscarEquipesComPontuacao();
+
+            int top = 10;
+            int rank = 1;
+            foreach (DataRow row in dtEquipes.Rows)
+            {
+                int idEquipe = (int)row["id_equipe"];
+                string nomeEquipe = row["nome_equipe"].ToString();
+                int pontosEquipe = row["pontos"] != DBNull.Value ? Convert.ToInt32(row["pontos"]) : 0;
+
+                List<MembroEquipe> membros = equipesRanking.BuscarMembrosEquipe(idEquipe);
+
+                int alturaPainel = 90;
+
+                Panel equipePanel = new Panel
+                {
+                    Width = panelRankingEquipes.Width - 40,
+                    Height = alturaPainel,
+                    BackColor = Color.White,
+                    Top = top,
+                    Left = 10,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Tag = idEquipe
+                };
+
+                PictureBox picIcone = new PictureBox
+                {
+                    Width = 44,
+                    Height = 44,
+                    Left = 10,
+                    Top = 20,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Image = null
+                };
+                if (rank == 1)
+                    picIcone.Image = Properties.Resources.icon_ranking_1;
+                else if (rank == 2)
+                    picIcone.Image = Properties.Resources.icon_ranking_2;
+                else if (rank == 3)
+                    picIcone.Image = Properties.Resources.icon_ranking_3;
+                equipePanel.Controls.Add(picIcone);
+
+                Label lblRank = new Label
+                {
+                    Text = $"#{rank}",
+                    Font = new Font("Segoe UI", 12, FontStyle.Bold),
+                    Left = picIcone.Right + 10,
+                    Top = 18,
+                    ForeColor = Color.Black,
+                    AutoSize = true
+                };
+                equipePanel.Controls.Add(lblRank);
+
+                Label lblNomeEquipe = new Label
+                {
+                    Text = nomeEquipe,
+                    Font = new Font("Segoe UI", 13, FontStyle.Bold),
+                    Left = picIcone.Right + 60,
+                    Top = 18,
+                    AutoSize = true
+                };
+                equipePanel.Controls.Add(lblNomeEquipe);
+
+                Label lblPontos = new Label
+                {
+                    Text = "Pontuação: " + pontosEquipe,
+                    Font = new Font("Segoe UI", 11, FontStyle.Regular),
+                    ForeColor = Color.DodgerBlue,
+                    Left = picIcone.Right + 60,
+                    Top = lblNomeEquipe.Bottom + 5,
+                    AutoSize = true
+                };
+                equipePanel.Controls.Add(lblPontos);
+
+                int numMembrosParaMostrar = Math.Min(membros.Count, 3);
+                int leftFoto = equipePanel.Width - 40 - 35 * numMembrosParaMostrar;
+
+                ToolTip toolTipMembros = new ToolTip();
+
+                for (int i = 0; i < numMembrosParaMostrar; i++)
+                {
+                    PictureBox picMembro = new PictureBox
+                    {
+                        Image = membros[i].FotoPerfil,
+                        SizeMode = PictureBoxSizeMode.StretchImage,
+                        Width = 34,
+                        Height = 34,
+                        Left = leftFoto + (i * 35),
+                        Top = 25,
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Cursor = Cursors.Hand
+                    };
+
+                    toolTipMembros.SetToolTip(picMembro, membros[i].Nome);
+
+                    equipePanel.Controls.Add(picMembro);
+                }
+
+                panelRankingEquipes.Controls.Add(equipePanel);
+
+                top += equipePanel.Height + 10;
+                rank++;
+            }
+        }
+
+        // Eventos e métodos inalterados seguem abaixo
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            var funcionario = Sessao.FuncionarioLogado;
+            var admin = Sessao.AdminLogado;
+
+            if (funcionario != null)
+            {
+                Home h = new Home();
+                h.Show();
+                this.Hide();
+            }
+            else if (admin != null)
+            {
+                HomeAdm t_equipeAdmin = new HomeAdm();
+                t_equipeAdmin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nenhum usuário logado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnEquipes_Click(object sender, EventArgs e)
+        {
+            var funcionario = Sessao.FuncionarioLogado;
+            var admin = Sessao.AdminLogado;
+
+            if (funcionario != null)
+            {
+                PesquisaEquipes t_equipe = new PesquisaEquipes();
+                t_equipe.Show();
+                this.Hide();
+            }
+            else if (admin != null)
+            {
+                PesquisaEquipes t_equipeAdmin = new PesquisaEquipes();
+                t_equipeAdmin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nenhum usuário logado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnRanking_Click(object sender, EventArgs e)
+        {
+            var funcionario = Sessao.FuncionarioLogado;
+            var admin = Sessao.AdminLogado;
+
+            if (funcionario != null)
+            {
+                Ranking_Equipes t_equipe = new Ranking_Equipes();
+                t_equipe.Show();
+                this.Hide();
+            }
+            else if (admin != null)
+            {
+                Ranking_Equipes t_equipeAdmin = new Ranking_Equipes();
+                t_equipeAdmin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nenhum usuário logado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Sessao.FuncionarioLogado = null;
+            Sessao.AdminLogado = null;
+
+            Form1 t_incial = new Form1();
+            t_incial.Show();
+            this.Hide();
+        }
+
+        private void lblPlanejamento_Click(object sender, EventArgs e)
+        {
+            var funcionario = Sessao.FuncionarioLogado;
+            var admin = Sessao.AdminLogado;
+
+            if (funcionario != null)
+            {
+                Planejamento t_equipe = new Planejamento();
+                t_equipe.Show();
+                this.Hide();
+            }
+            else if (admin != null)
+            {
+                Planejamento t_equipeAdmin = new Planejamento();
+                t_equipeAdmin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nenhum usuário logado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void label40_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void lblMembros_Click(object sender, EventArgs e)
+        {
+            var funcionario = Sessao.FuncionarioLogado;
+            var admin = Sessao.AdminLogado;
+
+            if (funcionario != null)
+            {
+                Integrantes_Equipe IE = new Integrantes_Equipe();
+                IE.Show();
+                this.Hide();
+            }
+            else if (admin != null)
+            {
+                Integrantes_Equipe IE = new Integrantes_Equipe();
+                IE.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nenhum usuário logado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void btnEstatisticas_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void lblGeral_Click(object sender, EventArgs e)
+        {
+            var funcionario = Sessao.FuncionarioLogado;
+            var admin = Sessao.AdminLogado;
+
+            if (funcionario != null)
+            {
+                Chat_geral_equipes t_equipe = new Chat_geral_equipes();
+                t_equipe.Show();
+                this.Hide();
+            }
+            else if (admin != null)
+            {
+                Chat_geral_equipes t_equipeAdmin = new Chat_geral_equipes();
+                t_equipeAdmin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nenhum usuário logado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void lblRanking_Click(object sender, EventArgs e)
+        {
+            var funcionario = Sessao.FuncionarioLogado;
+            var admin = Sessao.AdminLogado;
+
+            if (funcionario != null)
+            {
+                Ranking_Equipes t_equipe = new Ranking_Equipes();
+                t_equipe.Show();
+                this.Hide();
+            }
+            else if (admin != null)
+            {
+                Ranking_Equipes t_equipeAdmin = new Ranking_Equipes();
+                t_equipeAdmin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nenhum usuário logado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void lblTarefas_Click(object sender, EventArgs e)
+        {
+            var funcionario = Sessao.FuncionarioLogado;
+            var admin = Sessao.AdminLogado;
+
+            if (funcionario != null)
+            {
+                Tarefas_Pendentes t_equipe = new Tarefas_Pendentes();
+                t_equipe.Show();
+                this.Hide();
+            }
+            else if (admin != null)
+            {
+                AvaliaçãoTarefaAdmin t_equipeAdmin = new AvaliaçãoTarefaAdmin();
+                t_equipeAdmin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Nenhum usuário logado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
---
+++ Desktop/Dev4Tech/Dev4Tech/pesquisaEquipes.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Dev4Tech
+{
+    partial class PesquisaEquipes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPesquisaEquipe = new System.Windows.Forms.TextBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.btnEquipe = new System.Windows.Forms.PictureBox();
+            this.btnCalendar = new System.Windows.Forms.PictureBox();
+            this.btnRanking = new System.Windows.Forms.PictureBox();
+            this.btnLogout = new System.Windows.Forms.PictureBox();
+            this.btnConfig = new System.Windows.Forms.PictureBox();
+            this.btnHome = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.filtroEquipes = new System.Windows.Forms.ComboBox();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.panelEquipes = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEquipe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCalendar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRanking)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnConfig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 54);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "WORKFLOW";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(341, 187);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 19);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "EQUIPES";
+            // 
+            // txtPesquisaEquipe
+            // 
+            this.txtPesquisaEquipe.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesquisaEquipe.Location = new System.Drawing.Point(345, 207);
+            this.txtPesquisaEquipe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPesquisaEquipe.Name = "txtPesquisaEquipe";
+            this.txtPesquisaEquipe.Size = new System.Drawing.Size(492, 26);
+            this.txtPesquisaEquipe.TabIndex = 30;
+            this.txtPesquisaEquipe.Text = "Pesquisar equipe";
+            this.txtPesquisaEquipe.Click += new System.EventHandler(this.txtPesquisaEquipe_Click);
+            this.txtPesquisaEquipe.TextChanged += new System.EventHandler(this.txtPesquisaEquipe_TextChanged);
+            this.txtPesquisaEquipe.Leave += new System.EventHandler(this.txtPesquisarEquipe_Leave);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox11.BackgroundImage = global::Dev4Tech.Properties.Resources.icon_perfil;
+            this.pictureBox11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox11.Location = new System.Drawing.Point(17, 753);
+            this.pictureBox11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(32, 28);
+            this.pictureBox11.TabIndex = 28;
+            this.pictureBox11.TabStop = false;
+            // 
+            // btnEquipe
+            // 
+            this.btnEquipe.BackColor = System.Drawing.Color.Blue;
+            this.btnEquipe.BackgroundImage = global::Dev4Tech.Properties.Resources.icon_equip;
+            this.btnEquipe.Location = new System.Drawing.Point(17, 65);
+            this.btnEquipe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEquipe.Name = "btnEquipe";
+            this.btnEquipe.Size = new System.Drawing.Size(32, 28);
+            this.btnEquipe.TabIndex = 27;
+            this.btnEquipe.TabStop = false;
+            this.btnEquipe.Click += new System.EventHandler(this.btnEquipe_Click);
+            // 
+            // btnCalendar
+            // 
+            this.btnCalendar.BackColor = System.Drawing.Color.Blue;
+            this.btnCalendar.BackgroundImage = global::Dev4Tech.Properties.Resources.icon_calendar;
+            this.btnCalendar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCalendar.Location = new System.Drawing.Point(17, 113);
+            this.btnCalendar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.Size = new System.Drawing.Size(32, 28);
+            this.btnCalendar.TabIndex = 26;
+            this.btnCalendar.TabStop = false;
+            this.btnCalendar.Click += new System.EventHandler(this.btnCalendar_Click);
+            // 
+            // btnRanking
+            // 
+            this.btnRanking.BackColor = System.Drawing.Color.Blue;
+            this.btnRanking.BackgroundImage = global::Dev4Tech.Properties.Resources.icon_ranking;
+            this.btnRanking.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRanking.Location = new System.Drawing.Point(17, 164);
+            this.btnRanking.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRanking.Name = "btnRanking";
+            this.btnRanking.Size = new System.Drawing.Size(32, 28);
+            this.btnRanking.TabIndex = 25;
+            this.btnRanking.TabStop = false;
+            this.btnRanking.Click += new System.EventHandler(this.btnRanking_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.Blue;
+            this.btnLogout.BackgroundImage = global::Dev4Tech.Properties.Resources.Nav_Icon_Item;
+            this.btnLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLogout.Location = new System.Drawing.Point(17, 700);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(32, 28);
+            this.btnLogout.TabIndex = 24;
+            this.btnLogout.TabStop = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.BackColor = System.Drawing.Color.Blue;
+            this.btnConfig.BackgroundImage = global::Dev4Tech.Properties.Resources.icon_config;
+            this.btnConfig.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnConfig.Location = new System.Drawing.Point(17, 665);
+            this.btnConfig.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(32, 28);
+            this.btnConfig.TabIndex = 23;
+            this.btnConfig.TabStop = false;
+            this.btnConfig.Click += new System.EventHandler(this.btnConfig_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.Blue;
+            this.btnHome.BackgroundImage = global::Dev4Tech.Properties.Resources.icon_Home;
+            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHome.Location = new System.Drawing.Point(17, 17);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(32, 28);
+            this.btnHome.TabIndex = 22;
+            this.btnHome.TabStop = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 847);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // filtroEquipes
+            // 
+            this.filtroEquipes.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filtroEquipes.FormattingEnabled = true;
+            this.filtroEquipes.Location = new System.Drawing.Point(844, 207);
+            this.filtroEquipes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.filtroEquipes.Name = "filtroEquipes";
+            this.filtroEquipes.Size = new System.Drawing.Size(191, 27);
+            this.filtroEquipes.TabIndex = 63;
+            this.filtroEquipes.SelectedIndexChanged += new System.EventHandler(this.filtroEquipes_SelectedIndexChanged);
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiltrar.Location = new System.Drawing.Point(1040, 207);
+            this.btnFiltrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(75, 26);
+            this.btnFiltrar.TabIndex = 64;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // panelEquipes
+            // 
+            this.panelEquipes.Location = new System.Drawing.Point(345, 261);
+            this.panelEquipes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelEquipes.Name = "panelEquipes";
+            this.panelEquipes.Size = new System.Drawing.Size(769, 468);
+            this.panelEquipes.TabIndex = 68;
+            this.panelEquipes.Paint += new System.Windows.Forms.PaintEventHandler(this.panelEquipes_Paint);
+            // 
+            // PesquisaEquipes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1685, 838);
+            this.Controls.Add(this.panelEquipes);
+            this.Controls.Add(this.btnFiltrar);
+            this.Controls.Add(this.filtroEquipes);
+            this.Controls.Add(this.txtPesquisaEquipe);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.btnEquipe);
+            this.Controls.Add(this.btnCalendar);
+            this.Controls.Add(this.btnRanking);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnConfig);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "PesquisaEquipes";
+            this.Text = "Equipes";
+            this.Load += new System.EventHandler(this.PesquisaEquipes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEquipe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCalendar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRanking)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnConfig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox btnEquipe;
+        private System.Windows.Forms.PictureBox btnCalendar;
+        private System.Windows.Forms.PictureBox btnRanking;
+        private System.Windows.Forms.PictureBox btnLogout;
+        private System.Windows.Forms.PictureBox btnConfig;
+        private System.Windows.Forms.PictureBox btnHome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPesquisaEquipe;
+        private System.Windows.Forms.ComboBox filtroEquipes;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.FlowLayoutPanel panelEquipes;
+    }
+}
